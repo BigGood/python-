feae0166be0d0a86796df7bd23081e8aa65f3f88
sun <srwl181818@gmail.com>
2017-03-21 22:40:47 +0800

    submit

---
+++ simulation.py
@@ -0,0 +1,46 @@
+from flask import Flask,jsonify,make_response
+import urllib.request
+import os
+import time
+import datetime
+import json
+
+app = Flask(__name__)
+url="http://stock.gtimg.cn/data/index.php?appn=detail&action=download&c=%s&d=%s"
+date="20170317"
+symbol="sz002653"
+
+@app.route('/')
+def hello_world():
+    return 'Hello '
+def response(data):
+    rst = make_response(data)
+    rst.headers['Access-Control-Allow-Origin'] = '*'
+    return rst
+@app.route('/test',methods=['POST', 'GET'])
+def test():
+    # test={"销量":[5, 20, 36, 10, 10, 20]}
+    # rst = make_response(jsonify(getdataByDay(symbol,date)))
+    # rst.headers['Access-Control-Allow-Origin'] = '*'
+    msg = getdataByDay(symbol,date)
+    if msg=="暂无数据":
+        return response("{'cdoe':'1'}")
+    lines=msg.split("\n")
+    if len(lines)<=1:
+        return response("{'cdoe':'1'}")
+    day_time=[]
+    day_price=[]
+    for index in range(len(lines)):
+        cows=lines[index].split("\t")
+        day_time.append(cows[0])
+        day_price.append(cows[1])
+    data={"date":day_time,"price":day_price}
+    return response(json.dumps(data))
+def getdataByDay(code,date):
+    req = urllib.request.Request(url % (code,date),method="GET")
+    msg=urllib.request.urlopen(req).read().decode("GBK")
+    return msg
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
